979820fd4ccb9e4d57410c9c43973713aebd43ca
siya-ng <ngsiya90@gmail.com>
2018-02-07 17:24:03 +0800

    added information

--- src/components/Information.js
+++ src/components/Information.js
@@ -1,58 +1,69 @@
 import React from 'react';
+import './Information.css';
 
 const Information = ({currentTheme}) => {
   return (
     <div>
     <div className={"portfolio portfolio-" + currentTheme.userTheme} id="portfolio">
       <div className="">
-        <h2 className={"text-uppercase text-" + currentTheme.userTheme}>Portfolio</h2>
+        <h2 className={"text-uppercase text-center text-" + currentTheme.userTheme}>Portfolio</h2>
         <br/>
         <div className="row">
-          <div className="col-md-6 col-lg-6">
-            <a className="portfolio-item d-block ml-auto" href="#portfolio-modal-1">
-              <div className="portfolio-item-caption d-flex position-absolute h-100 w-100">
-                <div className="portfolio-item-caption-content my-auto w-100 text-center text-white">
-                  <i className="fa fa-search-plus fa-3x"></i>
+          <div className="col-md-6 col-lg-6 portfolio-project">
+            <div className="portfolio-item d-block ml-auto">
+              <a className="portfolio-item d-block ml-auto" >
+                <div className="portfolio-item-caption d-flex position-absolute h-100 w-100">
+                  <div className="portfolio-item-caption-content my-auto w-100 text-center text-white">
+                    <i className="fa fa-search-plus fa-3x"></i>
+                  </div>
                 </div>
-              </div>
-              <img className="img-fluid" src="img/portfolio/project1.png" alt=""/>
-            </a>
+                <img className="img-fluid ml-auto" src="img/portfolio/project1.png" alt=""/>
+              </a>
+            </div>
           </div>
-          <div className="col-md-6 col-lg-6">
-            <a className="portfolio-item d-block mr-auto" href="#portfolio-modal-2">
-              <div className="portfolio-item-caption d-flex position-absolute h-100 w-100">
-                <div className="portfolio-item-caption-content my-auto w-100 text-center text-white">
-                  <i className="fa fa-search-plus fa-3x"></i>
+          <div className="col-md-6 col-lg-6 portfolio-project">
+            <div className="portfolio-item d-block mr-auto" >
+              <a className="portfolio-item d-block mr-auto" >
+                <div className="portfolio-item-caption d-flex position-absolute h-100 w-100">
+                  <div className="portfolio-item-caption-content my-auto w-100 text-center text-white">
+                    <i className="fa fa-search-plus fa-3x"></i>
+                  </div>
                 </div>
-              </div>
-              <img className="img-fluid" src="img/portfolio/project2.png" alt=""/>
-            </a>
+                <img className="img-fluid" src="img/portfolio/project2.png" alt=""/>
+              </a>
+            </div>
           </div>
-          <div className="col-md-6 col-lg-6">
-            <a className="portfolio-item d-block ml-auto" href="#portfolio-modal-3">
-              <div className="portfolio-item-caption d-flex position-absolute h-100 w-100">
-                <div className="portfolio-item-caption-content my-auto w-100 text-center text-white">
-                  <i className="fa fa-search-plus fa-3x"></i>
+          <div className="col-md-6 col-lg-6 portfolio-project">
+            <div className="portfolio-item d-block ml-auto" >
+              <a className="portfolio-item d-block ml-auto">
+                <div className="portfolio-item-caption d-flex position-absolute h-100 w-100">
+                  <div className="portfolio-item-caption-content my-auto w-100 text-center text-white">
+                    <i className="fa fa-search-plus fa-3x"></i>
+                  </div>
                 </div>
-              </div>
-              <img className="img-fluid" src="img/portfolio/project3.png" alt=""/>
-            </a>
+                <img className="img-fluid" src="img/portfolio/project3.png" alt=""/>
+              </a>
+
+            </div>
           </div>
-          <div className="col-md-6 col-lg-6">
-            <a className="portfolio-item d-block mr-auto" href="#portfolio-modal-4">
-              <div className="portfolio-item-caption d-flex position-absolute h-100 w-100">
-                <div className="portfolio-item-caption-content my-auto w-100 text-center text-white">
-                  <i className="fa fa-search-plus fa-3x"></i>
-                </div>
+          <div className="col-md-6 col-lg-6 portfolio-project">
+              <div className="portfolio-item d-block mr-auto" >
+
+                <a className="portfolio-item d-block mr-auto" >
+                  <div className="portfolio-item-caption d-flex position-absolute h-100 w-100">
+                    <div className="portfolio-item-caption-content my-auto w-100 text-center text-white">
+                      <i className="fa fa-search-plus fa-3x"></i>
+                    </div>
+                  </div>
+                  <img className="img-fluid" src="img/portfolio/project4.png" alt=""/>
+                </a>
               </div>
-              <img className="img-fluid" src="img/portfolio/project4.png" alt=""/>
-            </a>
           </div>
 
         </div>
       </div>
     </div>
-    <div className={" mb-0 section bg-" + currentTheme.userTheme + " text-secondary-" + currentTheme.userTheme} id="about">
+    <div className={" mb-0 section text-center  bg-" + currentTheme.userTheme + " text-secondary-" + currentTheme.userTheme} id="about">
       <div>
         <img className="img-fluid mb-5 d-block mx-auto" src="img/profilepic.png" alt=""/>
 
@@ -68,17 +79,19 @@ const Information = ({currentTheme}) => {
         </div>
 
         <div className="row">
-          <div className="col-lg-4 ml-auto">
+          <div className="col-lg-4 ml-auto portion">
             <h5 className="text-center text-white">Language:</h5>
             <h6 className="text-center text-white">HTML | CSS | Javascript | Ruby </h6>
           </div>
-          <div className="col-lg-4 mr-auto">
+          <div className="col-lg-4 mr-auto portion">
             <h5 className="text-center text-white">Framework and Library:</h5>
             <h6 className="text-center text-white"> Jquery | Node.js | Express | Rails | React </h6>
           </div>
         </div>
       </div>
     </div>
+
+
   </div>
 
 
